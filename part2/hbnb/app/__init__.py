5fa81001cbd16995325679bd48ab471161ab81c8
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-02-25 14:50:10 +0000

    Added lines for amenity

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -5,6 +5,7 @@ Configure Flask et Flask-RESTX.
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenities_ns
 
 def create_app():
     """
@@ -14,4 +15,5 @@ def create_app():
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
     api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
     return app
