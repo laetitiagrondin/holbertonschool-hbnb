21506e44099b4b65d988d072249d661019686370
luidgiwtsn <lwbordeaux@gmail.com>
2026-02-26 09:39:30 +0100

    add .gitignore|app/__init__.py

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -6,10 +6,7 @@ from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
-<<<<<<< HEAD
-=======
 from app.api.v1.places import api as places_ns
->>>>>>> 22fff6f (Add app/__init__.py from app.api.v1.amenities import api as amenities_ns|from app.api.v1.places import api as places_ns|api.add_namespace(amenities_ns, path='/api/v1/amenities')|api.add_namespace(places_ns, path='/api/v1/places'))
 
 def create_app():
     """
@@ -20,8 +17,5 @@ def create_app():
 
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
-<<<<<<< HEAD
-=======
     api.add_namespace(places_ns, path='/api/v1/places')
->>>>>>> 22fff6f (Add app/__init__.py from app.api.v1.amenities import api as amenities_ns|from app.api.v1.places import api as places_ns|api.add_namespace(amenities_ns, path='/api/v1/amenities')|api.add_namespace(places_ns, path='/api/v1/places'))
     return app
