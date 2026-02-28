ba36bc7754fa2957367aadbaf26016169204d151
luidgiwtsn <lwbordeaux@gmail.com>
2026-02-28 09:25:10 +0100

    fix problem test users.py

--- part2/hbnb/app/api/v1/users.py
+++ part2/hbnb/app/api/v1/users.py
@@ -22,16 +22,20 @@ class UserList(Resource):
     def post(self):
         """Register a new user"""
         user_data = api.payload
-        existing_user = facade.get_user_by_email(user_data['email'])
-        if existing_user:
-            return {'error': 'Email already registered'}, 400
-        new_user = facade.create_user(user_data)
-        return {
+        try:
+            existing_user = facade.get_user_by_email(user_data['email'])
+            if existing_user:
+                return {'error': 'Email already registered'}, 400
+            
+            new_user = facade.create_user(user_data)
+            return {
                 'id': new_user.id,
                 'first_name': new_user.first_name,
                 'last_name': new_user.last_name,
                 'email': new_user.email
-                }, 201
+            }, 201
+        except ValueError as e:
+            return {'error': str(e)}, 400 # Transforme l'erreur du modèle en 400
 
     @api.response(200, 'List of users retrieved successfully')
     def get(self):
