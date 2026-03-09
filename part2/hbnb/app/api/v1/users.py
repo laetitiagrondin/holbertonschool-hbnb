c9aa59e46a1742735ff9e4b9f42ef7f7c6d45fd7
luidgiwtsn <lwbordeaux@gmail.com>
2026-03-09 13:52:33 +0100

    Add ajout du hachage avant stockage et du hachage du password est fourni et vérification mail unique

--- part2/hbnb/app/api/v1/users.py
+++ part2/hbnb/app/api/v1/users.py
@@ -1,16 +1,15 @@
 from flask_restx import Namespace, Resource, fields
 from app.services import facade
+from app.extensions import bcrypt
 
 api = Namespace('users', description='User operations')
 
 # Define the user model for input validation and documentation
 user_model = api.model('User', {
-    'first_name': fields.String(required=True,
-                                description='First name of the user'),
-    'last_name': fields.String(required=True,
-                               description='Last name of the user'),
+    'first_name': fields.String(required=True, description='First name of the user'),
+    'last_name': fields.String(required=True, description='Last name of the user'),
     'email': fields.String(required=True, description='Email of the user'),
-    'password': fields.String(required=False, description='Password of the user')
+    'password': fields.String(required=True, description='Password of the user')
 })
 
 
@@ -30,7 +29,13 @@ class UserList(Resource):
             existing_user = facade.get_user_by_email(user_data.get('email', ''))
             if existing_user:
                 return {'error': 'Email already registered'}, 400
-            
+
+            # Hachage du password avant stockage
+            if 'password' in user_data:
+                user_data['password'] = bcrypt.generate_password_hash(
+                    user_data['password']
+                ).decode('utf-8')
+
             new_user = facade.create_user(user_data)
             return {
                 'id': new_user.id,
@@ -54,19 +59,6 @@ class UserList(Resource):
                     })
         return {'users': users_list}, 200
 
-"""     @api.response(200, 'Liste des utilisateurs récupérée avec succès')
-    def get(self):
-
-        users = facade.get_all_users()
-        return [
-            {
-                "id": u.id,
-                "first_name": u.first_name,
-                "last_name": u.last_name,
-                "email": u.email
-            } for u in users
-        ], 200
- """
 
 @api.route('/<user_id>')
 class UserResource(Resource):
@@ -90,15 +82,27 @@ class UserResource(Resource):
     @api.response(400, 'Email already registered')
     @api.response(404, 'User not found')
     def put(self, user_id):
+        """mise a jour utilisateur"""
+        user_data = api.payload
         print("user_id:", user_id)
+        if not user_data:
+            return {'error': 'Invalid input data'}, 400
         try:
             user = facade.get_user(user_id)
             if not user:
                 return {'error': 'User not found'}, 404
-            existing_user = facade.get_user(user_id)
+            # Vérification email unique
+            existing_user = facade.get_user_by_email(user_data.get('email', ''))
             if existing_user and existing_user.id != user_id:
                 return {'error': 'User already registered'}, 400
-            updated_user = facade.update_user(user_id, api.payload)
+            
+             # Hachage du password si fourni
+            if 'password' in user_data:
+                user_data['password'] = bcrypt.generate_password_hash(
+                    user_data['password']
+                ).decode('utf-8')
+                
+            updated_user = facade.update_user(user_id, user_data)
             return {
                     'id': updated_user.id,
                     'first_name': updated_user.first_name,
