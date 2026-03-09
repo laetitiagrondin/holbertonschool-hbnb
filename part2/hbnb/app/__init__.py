18d4529782251034d82e55f2047341f9be1b2eab
luidgiwtsn <lwbordeaux@gmail.com>
2026-03-09 14:54:36 +0100

    Add de import auth_ns et de lextension.py jwt

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -4,7 +4,8 @@ Configure Flask et Flask-RESTX.
 """
 from flask import Flask
 from flask_restx import Api
-from app.extensions import bcrypt
+from app.extensions import bcrypt, jwt
+from app.api.v1.auth import api as auth_ns
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
@@ -19,9 +20,11 @@ def create_app(config_class="config.DevelopmentConfig"):
     app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
     bcrypt.init_app(app)
+    jwt.init_app(app)
     
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
     return app
