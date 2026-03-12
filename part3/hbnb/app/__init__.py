be68d3296b05f2c658bf1f57ff08a9ecb2404e5e
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-03-12 08:52:51 +0000

    Import et initialisation de flask-bcrypt

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    bcrypt.init_app(app)
     return app
