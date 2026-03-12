7bff801f7a8043f4c1ebb889fc50c5c181fae014
luidgiwtsn <lwbordeaux@gmail.com>
2026-03-12 10:03:05 +0100

    refactoring: import de SQLAlchemyRepository dans la facade

--- part3/part3/hbnb/app/services/facade.py
+++ part3/part3/hbnb/app/services/facade.py
@@ -4,13 +4,13 @@ Module implémentant le pattern Facade pour coordonner les couches.
 from app.models.user import User
 from app.models.amenity import Amenity
 from app.models.place import Place
-from app.persistence.repository import InMemoryRepository
+from app.persistence.repository import InMemoryRepository, SQLAlchemyRepository
 
 
 class HBnBFacade:
     """ Facade pour gérer la communication entre les couches. """
     def __init__(self):
-        self.user_repo = InMemoryRepository()
+        self.user_repo = SQLAlchemyRepository(User)
         self.place_repo = InMemoryRepository()
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
