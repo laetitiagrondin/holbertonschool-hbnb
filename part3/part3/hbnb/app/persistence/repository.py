b90426f82f7250c69c88fb059813d6d24b13cbbb
luidgiwtsn <lwbordeaux@gmail.com>
2026-03-12 10:01:00 +0100

    fonctionnalité: ajout de la classe SQLAlchemyRepository dans Repository

--- part3/part3/hbnb/app/persistence/repository.py
+++ part3/part3/hbnb/app/persistence/repository.py
@@ -4,6 +4,7 @@ Module définissant l'interface du dépôt et son implémentation en mémoire.
 
 
 from abc import ABC, abstractmethod
+from app.extensions import db
 
 class Repository(ABC):
     """ Interface abstraite pour les dépôts de données. """
@@ -59,3 +60,46 @@ class InMemoryRepository(Repository):
 
     def get_by_attribute(self, attr_name, attr_value):
         return next((obj for obj in self._storage.values() if getattr(obj, attr_name) == attr_value), None)
+    
+
+
+class SQLAlchemyRepository(Repository):
+    """Implémentation SQLAlchemy de l'interface Repository."""
+
+    def __init__(self, model):
+        # Le modèle SQLAlchemy géré par ce dépôt (User, Place, etc.)
+        self.model = model
+
+    def add(self, obj):
+        """Ajoute un objet en base et valide la transaction."""
+        db.session.add(obj)
+        db.session.commit()
+
+    def get(self, obj_id):
+        """Récupère un objet par son identifiant UUID."""
+        return self.model.query.get(obj_id)
+
+    def get_all(self):
+        """Retourne tous les objets du modèle."""
+        return self.model.query.all()
+
+    def update(self, obj_id, data):
+        """Met à jour les attributs d'un objet et valide la transaction."""
+        obj = self.get(obj_id)
+        if obj:
+            for key, value in data.items():
+                setattr(obj, key, value)
+            db.session.commit()
+
+    def delete(self, obj_id):
+        """Supprime un objet de la base et valide la transaction."""
+        obj = self.get(obj_id)
+        if obj:
+            db.session.delete(obj)
+            db.session.commit()
+
+    def get_by_attribute(self, attr_name, attr_value):
+        """Filtre les objets par un attribut donné."""
+        return self.model.query.filter(
+            getattr(self.model, attr_name) == attr_value
+        ).first()
