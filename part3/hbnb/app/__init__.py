d551eea508f8bfdae5b7194ca3a31196ad42585f
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-03-12 13:13:58 +0000

    Import et initialisation de JWTManager

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 bcrypt = Bcrypt()
+jwt = JWTManager()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
     bcrypt.init_app(app)
+    jwt.init_app(app)
     return app
